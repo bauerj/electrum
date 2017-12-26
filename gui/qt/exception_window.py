ab8872013558d9d9ba6ee69cb2a8f25d51a85f75
Johann Bauer <bauerj@bauerj.eu>
2017-12-26 21:46:16 +0100

    Split crash reporter class In Qt related stuff and basic stuff.

--- gui/qt/exception_window.py
+++ gui/qt/exception_window.py
@@ -21,42 +21,23 @@
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import json
-import locale
+import sys
 import platform
-import traceback
 
-import requests
 from PyQt5.QtCore import QObject
 import PyQt5.QtCore as QtCore
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 
 from electrum.i18n import _
-import sys
-from electrum import ELECTRUM_VERSION
-
-issue_template = """<h2>Traceback</h2>
-<pre>
-{traceback}
-</pre>
-
-<h2>Additional information</h2>
-<ul>
-  <li>Electrum version: {electrum_version}</li>
-  <li>Operating system: {os}</li>
-  <li>Wallet type: {wallet_type}</li>
-  <li>Locale: {locale}</li>
-</ul>
-"""
-report_server = "https://crashhub.bauerj.eu/crash"
+from electrum.base_crash_reporter import BaseCrashReporter
 
 
-class Exception_Window(QWidget):
+class Exception_Window(BaseCrashReporter, QWidget):
     _active_window = None
 
     def __init__(self, main_window, exctype, value, tb):
-        self.exc_args = (exctype, value, tb)
+        BaseCrashReporter.__init__(self, exctype, value, tb)
         self.main_window = main_window
         QWidget.__init__(self)
         self.setWindowTitle('Electrum - ' + _('An Error Occured'))
@@ -64,25 +45,23 @@ class Exception_Window(QWidget):
 
         main_box = QVBoxLayout()
 
-        heading = QLabel('<h2>' + _('Sorry!') + '</h2>')
+        heading = QLabel('<h2>' + BaseCrashReporter.CRASH_TITLE + '</h2>')
         main_box.addWidget(heading)
-        main_box.addWidget(QLabel(_('Something went wrong while executing Electrum.')))
+        main_box.addWidget(QLabel(BaseCrashReporter.CRASH_MESSAGE))
 
-        main_box.addWidget(QLabel(
-            _('To help us diagnose and fix the problem, you can send us a bug report that contains useful debug '
-              'information:')))
+        main_box.addWidget(QLabel(BaseCrashReporter.REQUEST_HELP_MESSAGE))
 
         collapse_info = QPushButton(_("Show report contents"))
-        collapse_info.clicked.connect(lambda: QMessageBox.about(self, "Report contents", self.get_report_string()))
+        collapse_info.clicked.connect(lambda: QMessageBox.about(self, _("Report contents"), self.get_report_string()))
         main_box.addWidget(collapse_info)
 
-        main_box.addWidget(QLabel(_("Please briefly describe what led to the error (optional):")))
+        main_box.addWidget(QLabel(BaseCrashReporter.DESCRIBE_ERROR_MESSAGE))
 
         self.description_textfield = QTextEdit()
         self.description_textfield.setFixedHeight(50)
         main_box.addWidget(self.description_textfield)
 
-        main_box.addWidget(QLabel(_("Do you want to send this report?")))
+        main_box.addWidget(QLabel(BaseCrashReporter.ASK_CONFIRM_SEND))
 
         buttons = QHBoxLayout()
 
@@ -105,11 +84,8 @@ class Exception_Window(QWidget):
         self.show()
 
     def send_report(self):
-        report = self.get_traceback_info()
-        report.update(self.get_additional_info())
-        report = json.dumps(report)
-        response = requests.post(report_server, data=report)
-        QMessageBox.about(self, "Crash report", response.text)
+        response = BaseCrashReporter.send_report(self)
+        QMessageBox.about(self, _("Crash report"), response.text)
         self.close()
 
     def on_close(self):
@@ -118,47 +94,21 @@ class Exception_Window(QWidget):
         self.close()
 
     def show_never(self):
-        self.main_window.config.set_key("show_crash_reporter", False)
+        self.main_window.config.set_key(BaseCrashReporter.config_key, False)
         self.close()
 
     def closeEvent(self, event):
         self.on_close()
         event.accept()
 
-    def get_traceback_info(self):
-        exc_string = str(self.exc_args[1])
-        stack = traceback.extract_tb(self.exc_args[2])
-        readable_trace = "".join(traceback.format_list(stack))
-        id = {
-            "file": stack[-1].filename,
-            "name": stack[-1].name,
-            "type": self.exc_args[0].__name__
-        }
-        return {
-            "exc_string": exc_string,
-            "stack": readable_trace,
-            "id": id
-        }
+    def get_user_description(self):
+        return self.description_textfield.toPlainText()
 
-    def get_additional_info(self):
-        args = {
-            "electrum_version": ELECTRUM_VERSION,
-            "os": platform.platform(),
-            "wallet_type": "unknown",
-            "locale": locale.getdefaultlocale()[0],
-            "description": self.description_textfield.toPlainText()
-        }
-        try:
-            args["wallet_type"] = self.main_window.wallet.wallet_type
-        except:
-            # Maybe the wallet isn't loaded yet
-            pass
-        return args
+    def get_wallet_type(self):
+        return self.main_window.wallet.wallet_type
 
-    def get_report_string(self):
-        info = self.get_additional_info()
-        info["traceback"] = "".join(traceback.format_exception(*self.exc_args))
-        return issue_template.format(**info)
+    def get_os_version(self):
+        return platform.platform()
 
 
 def _show_window(*args):
@@ -171,7 +121,7 @@ class Exception_Hook(QObject):
 
     def __init__(self, main_window, *args, **kwargs):
         super(Exception_Hook, self).__init__(*args, **kwargs)
-        if not main_window.config.get("show_crash_reporter", default=True):
+        if not main_window.config.get(BaseCrashReporter.config_key, default=True):
             return
         self.main_window = main_window
         sys.excepthook = self.handler
